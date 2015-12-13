a23b51d9c76406d8b583ba27c5a9e4b2ca04ec06
Ken Ho <ken.ho@hotmail.co.uk>
2015-12-13 15:33:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YelpEsque::Application.config.secret_key_base = '0a549af9f7403b4ce44cb3e888ba3bd448b5d044cd9f058ea1d751f76c96aac05e51a1c1cb9089c865619ff193dfc7ad1b76e7a74096ad6ba871869ae7f1adad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YelpEsque::Application.config.session_store :cookie_store, key: '_yelp-esque_session'
