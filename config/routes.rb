b587a3a567af43a08f7528dcb39e6c0d2b9e8bcb
Ken Ho <ken.ho@hotmail.co.uk>
2015-12-22 22:13:48 +0000

    Insert user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 YelpEsque::Application.routes.draw do
   devise_for :users
+
   root 'places#index'
+
   resources :places do
     resources :comments, :only => :create
     resources :photos
   end
+
+  resources :users, :only => :show
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
