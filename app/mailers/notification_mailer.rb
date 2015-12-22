8bf6c31f2725b901bad59bd93db340e9de2e0e86
Ken Ho <ken.ho@hotmail.co.uk>
2015-12-22 17:06:22 +0000

    Adjust mail notification to place_owner with dynamic content

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@yelp-esque-app.com"
 
-  def comment_added
-    mail(to: "ken.ho@hotmail.co.uk", subject: "YelpEsque! - Someone commented on your place!")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+         subject: "YelpEsque! - Someone commented on your post about #{@place.name}!")
   end
 end
